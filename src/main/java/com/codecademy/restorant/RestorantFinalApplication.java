a66578a6fbd3e8a06bc4d8e0e7e99d0408279a74
Gaspar <e972136@gmail.com>
2022-12-07 15:01:45 -0600

    Inicial

---
+++ src/main/java/com/codecademy/restorant/RestorantFinalApplication.java
@@ -0,0 +1,13 @@
+package com.codecademy.restorant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestorantFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestorantFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecademy/restorant/RestorantFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecademy.restorant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestorantFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
